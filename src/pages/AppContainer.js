dd4c75a1fc5475dd1de8d25c4246b2ae4fb27c0f
Neil Thawani <neilthawani@gmail.com>
2021-12-23 12:29:19 -0800

    Actual GH pages

--- src/pages/AppContainer.js
+++ src/pages/AppContainer.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 
 import { Route, Switch } from 'react-router-dom';
-import PrivateRoute from './PrivateRoute';
 import PropTypes from 'prop-types';
 
 const AppContainer = function(props) {
